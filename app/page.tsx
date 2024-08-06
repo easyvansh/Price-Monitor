0ec17d2b7c988a5a3bfeb4e89e63bbfed5ab3d47
Vansh Singh <vansh2@ualberta.ca>
2024-08-06 14:35:36 +0530

    Updated Carousel

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const Home = () => {
+  const allProducts = ['apple','samsung','xiaomi','lenovo'];
   return (
-    <div>Home</div>
-  )
-}
+    <>
+    
+    <section className="px-6 md:px-20 py-24 border-2 border-blue-700">
+      <div className="flex max-xl:flex-col gap-16">
+        <div className="flex flex-col justify-center">
+          <p className="small-text">
+            Smart Shopping Starts Here:
+            <Image
+              src="/assets/icons/arrow-right.svg"
+              alt="arrow-right"
+              width={16}
+              height={16}
+            />
+          </p>
+          <h1 className="head-text">
+            Unleash the Power of
+            <span className="text-blue-800"> PriceWise</span>
+          </h1>
+          <p className="mt-6">
+            Powerful, self-serve product and growth analytics to help you
+            convert, engage, and retain more.
+          </p>
+          SearchBar
+        </div>
+        Hero Carousel
+      </div>
+    </section>
+    
+    <section className="trending-section">
+     <h2 className="section-text">Trending</h2>
 
-export default Home
+     <div className="flex flex-wrap gap-x-8 gap-y-16">
+     {allProducts?.map((product) => (
+           <div>{product}</div>
+          ))}
+      </div>
+   </section>
+    </>
+  );
+};
+
+export default Home;
